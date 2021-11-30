abd84374f3647f047cc09a4bdd23ac3af0622098
Ori Hayun <hayun.ori@gmail.com>
2021-12-01 01:39:01 +0200

    server completed

---
+++ src/routes/propertyRoutes.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const Property = require('../models/property');
+
+const router = express.Router();
+
+router.get('/properties', (req, res) => {
+    const query = Property.find({})
+
+    query.select('address category originalPrice leaseData content coords favorite photos');
+
+    query.exec((err, property) => {
+        if (err) {
+            res.status(500).send()
+        }
+        res.send(property)
+    })
+})
+
+
+router.put('/properties/:id', async (req, res) => {
+    try {
+        const property = await Property.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        console.log(property)
+        if (!property) {
+            return res.status(404).send();
+        }
+
+        res.send(property);
+    } catch (e) {
+        console.log(e)
+        res.status(400).send(e)
+    }
+})
+
+module.exports = router;
